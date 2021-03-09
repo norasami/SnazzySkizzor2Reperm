98935d6d7368a5b002d81c6619a934e6f3db4ad5
Nora Luck-Jennings <1018949@student.sae.edu.au>
2021-03-09 15:49:27 +1100

    Implemented all necessary programming for skizzors and cutting hair, combined scripts into PlayerController
    Set up all the triggers, animations, bools, OnMouseDown, etc. needed to make the functionality of clicking the skizzors to enter cutting mode (wherein horizontal lines appear on the hair), and clicking on these lines to cut the hair to varying lengths, working now

--- SnazzySkizzor2Repermed/Assets/Scripts/PlayerController.cs
+++ SnazzySkizzor2Repermed/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    int depth = 1;
+    bool skizzorTime;
+    bool cuttingTime;
+    bool cutHere_01;
+    bool cutHere_02;
+    bool cutHere_03;
+    bool cutHere_04;
+    public int depth;
+    Animator cursorAnim;
+    Animator hairAnim;
+    GameObject cutTrig_01;
+    GameObject cutTrig_02;
+    GameObject cutTrig_03;
+    GameObject cutTrig_04;
     void Start()
     {
-        
+        cursorAnim = GameObject.FindWithTag("Player").GetComponent<Animator>();
+        hairAnim = GameObject.FindWithTag("Hair").GetComponent<Animator>();
+        cutTrig_01 = GameObject.FindWithTag("CutTrig_01");
+        cutTrig_02 = GameObject.FindWithTag("CutTrig_02");
+        cutTrig_03 = GameObject.FindWithTag("CutTrig_03");
+        cutTrig_04 = GameObject.FindWithTag("CutTrig_04");
+        cutTrig_01.GetComponent<BoxCollider2D>().enabled = false;
+        cutTrig_02.GetComponent<BoxCollider2D>().enabled = false;
+        cutTrig_03.GetComponent<BoxCollider2D>().enabled = false;
+        cutTrig_04.GetComponent<BoxCollider2D>().enabled = false;
     }
 
     // Update is called once per frame
@@ -17,35 +38,100 @@ public class PlayerController : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
 
         Vector3 wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, depth));
-
-        Vector3 playerPosition = transform.position;
-        /*if ((playerPosition.x + mousePos.x) < -5.29f)
-        {
-            wantedPos.x = -5.29f - playerPosition.x;
-        }
-        else if ((playerPosition.x + mousePos.x) > 4.42f)
-        {
-            wantedPos.x = 4.42f - playerPosition.x;
-        }
-        else
-        {*/
-            wantedPos = Camera.main.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, depth));
-        //}
-
-       /*if ((playerPosition.y + mousePos.y) < -2.52f)
-        {
-            wantedPos.y = -2.52f - playerPosition.y;
-        }
-        else if ((playerPosition.y + mousePos.y) > 1.18f)
-        {
-            wantedPos.y = 1.18f - playerPosition.y;
-        }
-        else
-        {*/
-            wantedPos = Camera.main.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, depth));
-        //}
-
-        transform.position = wantedPos;
         
+        transform.position = wantedPos;
+
+        //transform.rotation = Quaternion.LookRotation(mousePos, wantedPos);
+
+        if (skizzorTime == true)
+        {
+            cutTrig_01.GetComponent<BoxCollider2D>().enabled = true;
+            cutTrig_02.GetComponent<BoxCollider2D>().enabled = true;
+            cutTrig_03.GetComponent<BoxCollider2D>().enabled = true;
+            cutTrig_04.GetComponent<BoxCollider2D>().enabled = true;
+            cuttingTime = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Skizzors"))
+        {
+            skizzorTime = true;
+        }
+        else
+        {
+            skizzorTime = false;
+        }
+
+        if(other.CompareTag("CutTrig_01"))
+        {
+            cutHere_01 = true;
+        }
+        else
+        {
+            cutHere_01 = false;
+        }
+
+        if (other.CompareTag("CutTrig_02"))
+        {
+            cutHere_02 = true;
+        }
+        else
+        {
+            cutHere_02 = false;
+        }
+
+        if (other.CompareTag("CutTrig_03"))
+        {
+            cutHere_03 = true;
+        }
+        else
+        {
+            cutHere_03 = false;
+        }
+
+        if (other.CompareTag("CutTrig_04"))
+        {
+            cutHere_04 = true;
+        }
+        else
+        {
+            cutHere_04 = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if(skizzorTime == true)
+        {
+            cursorAnim.SetTrigger("skizzorTime");
+            hairAnim.SetTrigger("Haircut");
+        }
+
+        if(cuttingTime == true)
+        {
+            cursorAnim.SetTrigger("cutHere");
+        }
+
+        if(cutHere_01 == true)
+        {
+            hairAnim.SetTrigger("CutPoint1");
+        }
+
+        if (cutHere_02 == true)
+        {
+            hairAnim.SetTrigger("CutPoint2");
+        }
+
+        if (cutHere_03 == true)
+        {
+            hairAnim.SetTrigger("CutPoint3");
+        }
+
+        if (cutHere_04 == true)
+        {
+            hairAnim.SetTrigger("CutPoint4");
+        }
     }
 }
